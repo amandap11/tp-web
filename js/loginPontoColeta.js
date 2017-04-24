33458bcfa3663bf17f2f71d78045bb0693a975fa
ViniciusMP <viniciusloco18@hotmail.com>
2017-04-24 16:13:36 -0300

    Pagina de Ponto de Coleta feita e navegável

---
+++ js/loginPontoColeta.js
@@ -0,0 +1,8 @@
+let botaoEntrar = document.getElementById("entrar");
+
+botaoEntrar.addEventListener('click', function(){
+
+	window.location.href = "paginaPontoColeta.html";
+	//window.location.href = "minhaConta.html";
+	
+});
