558b272e0517ea8ae04c1f5805ac3febef6a738d
Amanda Pereira <afp1103@gmail.com>
2017-04-25 00:37:07 -0300

    correcao dos links de navegação

--- js/login.js
+++ js/login.js
@@ -2,7 +2,6 @@ let botaoEntrar = document.getElementById("entrar");
 
 botaoEntrar.addEventListener('click', function(){
 
-	window.location.href = "editarConta.html";
-	//window.location.href = "minhaConta.html";
+	window.location.href = "minhaContaDoador.html";
 	
 });
