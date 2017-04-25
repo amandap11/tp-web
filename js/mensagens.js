7469b491719457c32f4969f2293b7092982be624
unknown <brucoletinha@gmail.com>
2017-04-25 00:52:22 -0300

    subindo mensagens

---
+++ js/mensagens.js
@@ -0,0 +1,11 @@
+function SucessoComprovante() {
+	alert("Comprovante gerado com sucesso!");
+}
+
+function SucessoCadastro() {
+	alert("Cadastro feito com sucesso!");
+}
+
+function SucessoContato() {
+	alert("Mensagem recebida! Obrigado!");
+}
