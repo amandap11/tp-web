7226674845cca66491ad77e9b5508a7917e41779
Amanda Pereira <afp1103@gmail.com>
2017-06-19 19:00:52 -0700

    tentativa de inclusao do material design

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let express = require('express'),
 
 var path = require('path');
 //var hbs = require('handlebars');
-var hbs = require('express-hbs');
+//var hbs = require('express-hbs');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -11,7 +11,7 @@ app.set('view engine', 'hbs');
 
 // suponhamos que "/public" é uma pasta com
 // nossos arquivos estáticos
-app.use(express.static(__dirname + '/views'));
+//app.use(express.static(__dirname + '/views'));
 
 let server = app.listen(3000, function () {
   console.log('Escutando em: http://localhost:3000');
