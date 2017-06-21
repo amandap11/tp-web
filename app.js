a97b70cbe88959196ac0f8bbf13e092b5a3d44f9
Amanda Pereira <afp1103@gmail.com>
2017-06-20 22:23:37 -0300

    adicionei o fs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require('express'),
     app = express();
 
 var path = require('path');
+var fs = require('fs');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -10,6 +11,7 @@ app.set('view engine', 'hbs');
 // suponhamos que "/public" é uma pasta com
 // nossos arquivos estáticos
 //app.use(express.static(__dirname + '/views'));
+
 
 var db = {
 	classificacoes: JSON.parse(fs.readFileSync(__dirname + "/data/classificacao-specs.json")),
